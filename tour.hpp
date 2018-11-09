6f088955d45de848a6ed9dfac9e5b4874dcd0547
Daniel DiIorio <ddiiorio@my.bcit.ca>
2018-11-08 23:03:36 -0800

    Starting to add functions to tour class

--- tour.hpp
+++ tour.hpp
@@ -5,6 +5,7 @@
 #define GENETICALGORITHM_TOUR_HPP
 
 #include <vector>
+#include <algorithm>
 #include "city.hpp"
 
 
@@ -12,6 +13,15 @@ class tour {
 protected:
     vector<city> cities;
     int fitness;
+
+public:
+    tour();
+    ~tour() = default;
+    void shuffle_cities();
+    unsigned int get_distance_between_cities(); //calculate the as-the-crow-flies distance between two cities
+    unsigned int get_tour_distance(); //reports the distance between the cities as they are listed in a tour
+    unsigned int determine_fitness(); //determines the fitness of a tour
+    bool contains_city(city); //checks if a tour contains a specified city
 
 };
 
